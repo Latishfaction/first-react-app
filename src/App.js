1f85c3ddc2f5e7b7406a7c64063378269fb9df5a
Latishfaction <latishchambhare@gmail.com>
2023-08-15 17:19:39 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,66 @@
 import React from "react";
+import { useEffect,useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+
+// http://www.omdbapi.com/?i=tt3896198&apikey=6f8f4316
+const apikey = "6f8f4316";
+const URL_API = `http://www.omdbapi.com/?i=tt3896198&apikey=${apikey}`;
+
 
 const App=()=>{
+    const [movies,setmovies]=useState([]);
+    const [searchTerm,setSearchTerm] =useState("");
+    
+    const searchMovies = async(title)=>{
+        const response = await fetch(`${URL_API}&s=${title}`);
+        const data = await response.json();
+        setmovies(data.Search);
+    }
+    useEffect(()=>{
+        searchMovies(searchTerm);
+    },[])
+
     return(
-        <h1>Hello World !</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="Search for Movies"
+                    value={searchTerm}
+                    onChange={(e)=>{
+                        setSearchTerm(e.target.value);
+                    }}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="SearchIcon"
+                    onClick={ ()=>{searchMovies(searchTerm)}}
+                />
+            </div>
+            
+            {
+                movies.length>0?
+                (
+                <div className="container">
+                    {
+                        movies.map((movie)=>(
+                            <MovieCard movie={movie} />
+                        ))
+                    }
+                </div>
+
+                ):
+                (
+                    <div className="empty">
+                        <h2>No movies found.</h2>
+                    </div>
+                )
+            }
+        </div>
     )
 }
 
